216a86c9fe74c85250b34eefc8ca97172af142ea
Simon Reinsperger <reinsperger.smn@gmail.com>
2023-03-23 11:05:08 +0100

    deployment: debug

--- script.ts
+++ script.ts
@@ -3,13 +3,11 @@ import * as d3 from 'd3';
 import dataUrl from 'url:./data/data.csv';
 
 d3.csv(dataUrl).then((data) => {
-    console.log(data);
-    return;
     const { columns } = data;
     const table = d3.select('#table').append('table');
     const thead = table.append('thead');
     const tbody = table.append('tbody');
-
+    return;
     thead.append('tr')
         .selectAll('th')
         .data(data.columns)
